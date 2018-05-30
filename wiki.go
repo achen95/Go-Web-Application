4f05d2f4af102ceda54d2f450a3dcd18c73918e0
Amy Chen <Amy@Amys-MacBook.local>
2018-05-30 17:02:16 +0800

    Corrected mistakes in the data structures

--- wiki.go
+++ wiki.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"io/ioutil"
-	{
-)
 	"fmt"
 	"io/ioutil"
-}
+)
 
 type Page struct {
 	Title string
-	Body []byte
+	Body  []byte
 }
 
 func (p *Page) save() error {
@@ -18,7 +15,7 @@ func (p *Page) save() error {
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
